360cd2b0cb564423a918e86c61e8f3bb26dbeb51
Giselle <giselle.ft@hotmail.com>
2018-10-31 23:55:05 -0600

    conexion de botones con el resto de las vistas

--- MyDoctor/app/src/main/java/com/movil/android/mydoctor/PrincipalActivity.java
+++ MyDoctor/app/src/main/java/com/movil/android/mydoctor/PrincipalActivity.java
@@ -18,4 +18,24 @@ public class PrincipalActivity extends AppCompatActivity {
         Intent intentAltaMedicamento = new Intent(this, AgregarMedicamentoActivity.class);
         startActivity(intentAltaMedicamento);
     }
+
+    //Metodo del boton Ver calendario
+    public void verCalendario(View view){
+        Intent intentVerCalendario = new Intent(this, VerCalendarioActivity.class);
+        startActivity(intentVerCalendario);
+    }
+
+    //Metodo del boton Ver medicamentos
+    public void verMedicamentos(View view){
+        Intent intentVerMedicamentos = new Intent(this, VerMedicamentosActivity.class);
+        startActivity(intentVerMedicamentos);
+    }
+
+    //Metodo del boton Ver farmacias
+    public void verFarmacias(View view){
+        Intent intentVerFarmacias = new Intent(this, VerFarmaciasActivity.class);
+        startActivity(intentVerFarmacias);
+    }
+
+
 }
--- MyDoctor/app/src/main/java/com/movil/android/mydoctor/VerCalendarioActivity.java
+++ MyDoctor/app/src/main/java/com/movil/android/mydoctor/VerCalendarioActivity.java
@@ -2,12 +2,19 @@ package com.movil.android.mydoctor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.TimeZone;
 
 public class VerCalendarioActivity extends AppCompatActivity {
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_calendario);
+
+        calendarView = (CalendarView)findViewById(R.id.calendarView);
+        
     }
 }
