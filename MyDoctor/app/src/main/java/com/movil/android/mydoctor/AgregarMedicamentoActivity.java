d8d96bb65b989bb181a39b5eca51d982dba446fc
Giselle <giselle.ft@hotmail.com>
2018-10-31 20:09:07 -0600

    principal activity

---
+++ MyDoctor/app/src/main/java/com/movil/android/mydoctor/AgregarMedicamentoActivity.java
@@ -0,0 +1,13 @@
+package com.movil.android.mydoctor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarMedicamentoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_medicamento);
+    }
+}
