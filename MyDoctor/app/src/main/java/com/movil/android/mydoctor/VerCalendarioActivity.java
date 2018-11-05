567320e79a82332c64d21891042701dece6291e6
Giselle <giselle.ft@hotmail.com>
2018-11-04 18:04:59 -0600

    calendario muestra eventos

--- MyDoctor/app/src/main/java/com/movil/android/mydoctor/VerCalendarioActivity.java
+++ MyDoctor/app/src/main/java/com/movil/android/mydoctor/VerCalendarioActivity.java
@@ -1,20 +1,79 @@
 package com.movil.android.mydoctor;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import java.util.TimeZone;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import static android.R.layout.simple_list_item_1;
 
 public class VerCalendarioActivity extends AppCompatActivity {
-    private CalendarView calendarView;
+    CompactCalendarView compactCalendar;
+    ListView listView;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_calendario);
 
-        calendarView = (CalendarView)findViewById(R.id.calendarView);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        Date date = new Date();
+        actionBar.setTitle(dateFormatMonth.format(date));
+        compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        //Set an event for Teachers' Professional Day 2016 which is 21st of October
+        /*Para generar los timestamp https://www.epochconverter.com/ como ejemplo
+         * en esta parte se tienen que recuperar los timestamp y crearlos como eventos y añadirlos al calendario
+         * el timestamp esta en milisegundos
+         * AQUI SE LLENA EL CALENDARIO CON LOS EVENTOS
+         * hacer distincion de medicamentos con un codigo de colores*/
+        Event ev1 = new Event(Color.BLACK, 1541376000000L, "Medicamento 1");
+        Event ev2 = new Event(Color.BLACK, 1541462400000L, "Medicamento 2");
+        Event ev3 = new Event(Color.BLACK, 1541534400000L, "Medicamento 3");
+        compactCalendar.addEvent(ev1);
+        compactCalendar.addEvent(ev2);
+        compactCalendar.addEvent(ev3);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+                /*AQUI SE RECUPERAN LOS EVENTOS POR DIA PARA MOSTRAR LOS MENSAJES ADECUADOS*/
+                List<Event> listaEventos = compactCalendar.getEvents(dateClicked);
+                //Se crea un arreglo segun el numero de eventos recuperados
+                String [] infoEventos = new String[listaEventos.size()];
+                listView = (ListView) findViewById(R.id.listView);
+                //Se llena el arreglo con la informacion de cada evento
+                for (int i = 0 ; i < listaEventos.size(); i++){
+                    infoEventos[i] = listaEventos.get(i).getData().toString();
+                    //Toast.makeText(context, "Recordatorio: " + listaEventos.get(i).getData(), Toast.LENGTH_SHORT).show();
+                }
+                //Se crea el arreglo que se mostrará en el ListView
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, infoEventos);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
 
     }
 }
