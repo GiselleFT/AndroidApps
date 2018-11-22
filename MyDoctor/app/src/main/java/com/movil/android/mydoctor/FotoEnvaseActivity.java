da133df2278cdf7469d65f8ac2210442af570bda
samyarteaga <sam-y.barca@hotmail.com>
2018-11-21 18:11:13 -0600

    guarda fotos

--- MyDoctor/app/src/main/java/com/movil/android/mydoctor/FotoEnvaseActivity.java
+++ MyDoctor/app/src/main/java/com/movil/android/mydoctor/FotoEnvaseActivity.java
@@ -2,8 +2,10 @@ package com.movil.android.mydoctor;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,65 +20,57 @@ import java.util.Date;
 
 public class FotoEnvaseActivity extends AppCompatActivity {
     ImageView img;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foto_envase);
 
-        Button button = (Button)findViewById(R.id.buttonTomarFoto);
-        img = (ImageView)findViewById(R.id.imageView);
+        Button button = (Button) findViewById(R.id.buttonTomarFoto);
+        img = (ImageView) findViewById(R.id.imageView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(intent, 0);
+                // Ensure that there's a camera activity to handle the intent
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    System.out.println("//////////////////////////////////////////////////////////////////////////////");
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(null,
+                                "com.example.android.fileprovider",
+                                photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(intent, 0);
+                        System.out.println("Entra1 - "+photoFile.getName());
+                        System.out.println(""+photoFile.getAbsolutePath());
+                    }
+                }
             }
         });
-        /*if (ContextCompat.checkSelfPermission(FotoEnvaseActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(FotoEnvaseActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(FotoEnvaseActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
-        }
-        imgButton = (ImageButton)findViewById(R.id.imageButton);*/
-
     }
 
 
-
-
-    //Metodo para tomar la foto
-    static final int REQUEST_TAKE_PHOTO = 1;
-    private void tomarFoto(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        /*if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }*/
-        startActivityForResult(takePictureIntent, 0);
-    }
 
 
     //Metodo para crear un nombre unico de cada fotografia
     //se guarda en el dispositivo y un respaldo dentro del paaquete de instalacion de la app
     String mCurrentPhotoPath;
+
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "Backup_" + timeStamp + "_";
+        String imageFileName = "Envase_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
@@ -89,22 +83,16 @@ public class FotoEnvaseActivity extends AppCompatActivity {
     }
 
 
-
-
     //Para mostrar la vista previa de la foto tomada en un image view
     //static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //tomarFoto();
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap imageBitmap = (Bitmap)data.getExtras().get("data");
+        Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
         img.setImageBitmap(imageBitmap);
-        /*super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            img.setImageBitmap(imageBitmap);
-        }
-        */
+
+
     }
 
 
